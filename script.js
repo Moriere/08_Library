fcd02345ff86b968d983d5d49fdc953cd4ea1ad8
Moriere <kevin_a_b@hotmail.com>
2022-08-22 21:37:56 -0500

    On html: added a template for the rows to be added and deleted the dummy rows.
    On styles:  added styles for row isRead toggle and delete buttons. Edited the fields placeholder color an font.
    
    On script: created the addRow() function that adds a row using the template. The cycle that edits the textContent property works excepting for the read/unread button, a conditional to be added for that field that converts the boolean in the isRead property to the read / unread chain.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addBookToLibrary(form) {
     book.nPages = form.elements['book_pages'].value;
     book.isRead = document.querySelector('input[name="isRead"]:checked').value;
     myLibrary.push(book);
+    addRow();
 }
 
 function removeInputFields() {
@@ -25,7 +26,7 @@ function createDOMFields() {
     const dimmer = document.querySelector('#dimmer');
     dimmer.classList.add('dimmed');
 
-    const template = document.querySelector('#template');
+    const template = document.querySelector('#formTemplate');
     const templateClone = template.content.cloneNode(true);
     dimmer.appendChild(templateClone);
 
@@ -40,8 +41,31 @@ function createDOMFields() {
     });
 };
 
-/* POP-UP FIELD */
-
 let myLibrary = [];
 const addBookBtn = document.getElementById('addBook');
 addBookBtn.addEventListener('click', createDOMFields);
+
+function addRow() {
+    const table = document.querySelector('.table');
+    const template = document.querySelector('#rowTemplate');
+    const templateClone = template.content.cloneNode(true);
+    table.appendChild(templateClone);
+
+    const lastBookIndex = myLibrary.length - 1;
+
+    table.lastElementChild.classList.add(`${lastBookIndex}`);
+/*     table.lastElementChild.querySelector('.title').textContent = myLibrary[myLibrary.length - 1].title; */
+
+    let i = 1;
+    for(let key in myLibrary[lastBookIndex]) {
+        table.lastElementChild.querySelector(`.row :nth-child(${i + 1})`).textContent = myLibrary[lastBookIndex][key];
+        i++;
+    };
+};
+/* 
+after clicking submit
+run the function addRow
+    get the template, clone and create new row
+    get each of divs, edit node text content with myLibrary[i].key
+    add an event listener to the button with delete icon and the status toggle
+*/
